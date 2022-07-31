d677f5cf615195730cb709fe8a1037dffb6ebad6
r-khvstnv <r.khvastunov@gmail.com>
2022-07-31 15:05:53 +0500

    Changed App Theme

---
+++ app/src/main/java/com/rssll971/loancalculator/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.rssll971.loancalculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF626263)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_secondary = Color(0xffbf6f6f)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFEFBFF)
+val md_theme_light_onBackground = Color(0xFF1B1B1F)
+val md_theme_light_surface = Color(0xFFFEFBFF)
+val md_theme_light_onSurface = Color(0xFF1B1B1F)
+
+val md_theme_dark_primary = Color(0xFF383839)
+val md_theme_dark_onPrimary = Color(0xB3EFEFEF)
+val md_theme_dark_secondary = Color(0xFF8c4244)
+val md_theme_dark_onSecondary = Color(0xfefefefe)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_background = Color(0xFF1B1B1F)
+val md_theme_dark_onBackground = Color(0xFFE3E2E6)
+val md_theme_dark_surface = Color(0xFF1B1B1F)
+val md_theme_dark_onSurface = Color(0xFFE3E2E6)
