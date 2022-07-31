99643f57df9ebb115865cc3750918bef2a918e45
r-khvstnv <r.khvastunov@gmail.com>
2022-07-31 21:03:03 +0500

    Updated App Theme

--- app/src/main/java/com/rssll971/loancalculator/ui/theme/Theme.kt
+++ app/src/main/java/com/rssll971/loancalculator/ui/theme/Theme.kt
@@ -6,7 +6,22 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
+private val LightColors = lightColors(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    error = md_theme_light_error,
+    onError = md_theme_light_onError,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
+
+)
+
+
+private val DarkColors = darkColors(
     primary = md_theme_dark_primary,
     onPrimary = md_theme_dark_onPrimary,
     secondary = md_theme_dark_secondary,
@@ -19,25 +34,15 @@ private val DarkColorPalette = darkColors(
     onSurface = md_theme_dark_onSurface,
 )
 
-private val LightColorPalette = lightColors(
-    primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary,
-    secondary = md_theme_light_secondary,
-    onSecondary = md_theme_light_onSecondary,
-    error = md_theme_light_error,
-    onError = md_theme_light_onError,
-    background = md_theme_light_background,
-    onBackground = md_theme_light_onBackground,
-    surface = md_theme_light_surface,
-    onSurface = md_theme_light_onSurface,
-)
-
 @Composable
-fun LoanCalcTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
+fun LoanCalcTheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (!useDarkTheme) {
+        LightColors
     } else {
-        LightColorPalette
+        DarkColors
     }
 
     MaterialTheme(
